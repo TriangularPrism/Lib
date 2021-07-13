81fc61347293c1997b5a48f980cb13150427dbbc
TriangularPrism <87276395+TriangularPrism@users.noreply.github.com>
2021-07-13 20:02:54 +0000

    Another attempt at fixing it, for some reason it will not work. (Variables do change, does not work with comparing variables.)

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 print("1 for About Me, 2 for superhero")
-var = input()
 
-print(var)
+var = input()
 
 var1 = 1
 var2 = 2
 
-if not var == var1:
-  
-  if not var == var2:
-    while not var == 1 and not var == 2:
+if var1 == var:
+  print(var)
+else:
+
+  if var2 == var:
+     print(var)
+  else:
+
+    while not var == var1 and not var == var2:
       print("1 for About Me, 2 for superhero")
+
       var = input()
-      print(var)
+      if var1 == var:
+        print(var)
+      else:
+
+        if var2 == var:
+          print(var)   
 
 if var == 1:
 
@@ -28,16 +38,18 @@ if var == 1:
 
   print("My name is", string1, ", My favorite food is ", string2, ",my favorite day of the week is ", string3, ", my favorite city is ",string4, ", my favorite holiday is ", string5, "and my favorite game is ", string6, ". I", string7, ".")
 
-if var == 2:
+else:
 
-  print("My Superhero")
+    if var == 2:
 
-  string1 = input("What is your superhero's name?")
-  string2 = input("What city does your superhero protect?")
-  string3 = input("")
-  string4 = input("What is your superhero's power?")
-  string5 = input("What is your superhero's weakness?")
-  string6 = input("")
-  string7 = input("")
+      print("My Superhero")
 
-  print("My superhero's name is ", string1, ". The city that my superhero protects is ", string2, "d", string3, ". My superhero's power is ", string4, ". My superhero's weakness is ", string5, "d", string6, "d", string7, ".")
+      string1 = input("What is your superhero's name?")
+      string2 = input("What city does your superhero protect?")
+      string3 = input("")
+      string4 = input("What is your superhero's power?")
+      string5 = input("What is your superhero's weakness?")
+      string6 = input("")
+      string7 = input("")
+
+      print("My superhero's name is ", string1, ". The city that my superhero protects is ", string2, "d", string3, ". My superhero's power is ", string4, ". My superhero's weakness is ", string5, "d", string6, "d", string7, ".")
